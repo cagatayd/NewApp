c413fb77c03a92a6be922492e9a6248a1d797b12
cagatayd <32108835+cagatayd@users.noreply.github.com>
2021-06-30 17:01:45 +0300

    navigationsfinished

---
+++ NewApp/app/src/main/java/com/cagatay/newapp/view/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.cagatay.newapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.cagatay.newapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController=findNavController(R.id.fragment2)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+
+    }
+}
